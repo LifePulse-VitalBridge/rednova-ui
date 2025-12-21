b1017ce3f4a344e9e2d9a7e65dd0e3f7d37969d1
Ankit-K-Singh-0527 <ankitkumarsinghbhumihar20@gmail.com>
2025-12-21 13:23:02 +0530

    Resolving issue6

--- backend/services/call.js
+++ backend/services/call.js
@@ -1,7 +1,5 @@
 import twilio from 'twilio';
-import dotenv from 'dotenv';
 
-dotenv.config();
 // --- CONFIGURATION (Replace these with your details) ---
 
 // 1. From Twilio Console Dashboard (Account Info)
